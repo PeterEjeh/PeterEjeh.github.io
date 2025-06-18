e3328ff83461aea56952c7c6b83069795253e516
Peter Ejeh <peterejeh09@gmail.com>
2025-06-18 08:32:31 +0100

    update

---
+++ navbar.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('navbar-toggle');
+const menu = document.getElementById('navbar-menu');
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
